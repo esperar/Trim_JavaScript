9d7fd9d57ef863db814e14796770037bb2796ced
KIMHUEMANG <piyrw9754@gmail.com>
2022-07-13 21:09:23 +0900

    hacker

--- Hacker_News/app.js
+++ Hacker_News/app.js
@@ -4,36 +4,41 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-ajax.open('GET',NEWS_URL,false); // 데이터를 가져옴
-ajax.send(); // 데이터가 들어옴
+function getData(url){
+  ajax.open('GET',url,false); // 데이터를 가져옴
+  ajax.send(); // 데이터가 들어옴
 
-const newsFeed = JSON.parse(ajax.response);
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange',function (){
+  const id = location.hash.substr(1);
+
+  const newsContent = getData(CONTENT_URL.replace('@id',id));
+  const title = document.createElement('h1');
+
+  title.innerHTML = newsContent.title;
+  
+  content.appendChild(title);
+}); // # 뒤에 값이 바뀌면 실행
+
+
 for(let i = 0; i < 10; i++){
+  const div = document.createElement("div");
   const li = document.createElement('li');
   const a = document.createElement('a');
 
-  window.addEventListener('hashchange',function (){
-    location.hash.substr();
+  div.innerHTML = `
+  <li>
+    <a href = "#${newsFeed[i].id}">
+    ${newsFeed[i].title} (${newsFeed[i].comments_count})
+    </a>
+  </li>`;
 
-    ajax.open("GET",CONTENT_URL.replace('@id',id),false);
-    ajax.send();
-
-    const newsContent = JSON.parse(ajax.response);
-    const title = document.createElement('h1');
-
-    title.innerHTML = newsContent.title;
-    
-    content.appendChild(title);
-  }); // # 뒤에 값이 바뀌면 실행
-
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`; // 제목 , 댓글
-
-  a.addEventListener("click",function () {});
-  li.appendChild(a);
-  ul.appendChild(li);
+  ul.appendChild(div.firstElementChild);   
 }
 
 container.appendChild(ul);
