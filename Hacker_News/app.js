b99bf610900bdbb63ff0a86f910753407dd941f1
KIMHUEMANG <piyrw9754@gmail.com>
2022-07-14 15:45:49 +0900

    다음화 오류 수정1

--- Hacker_News/app.js
+++ Hacker_News/app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest(); // 출력 결과를 돌려줌
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = {
+  currentPage : 1,
+};
 
 function getData(url){
   ajax.open('GET',url,false); // 데이터를 가져옴
@@ -10,20 +13,20 @@ function getData(url){
 
   return JSON.parse(ajax.response);
 }
-
+// newsFeed
 function newsFeed(){
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
 
   newsList.push('<ul>');
 
-  for(let i = 0; i < 10; i++){
+  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
     const div = document.createElement("div");
 
     newsList.push(
     div.innerHTML = `
     <li>
-      <a href = "#${newsFeed[i].id}">
+      <a href = "#/show/${newsFeed[i].id}">
         ${newsFeed[i].title} (${newsFeed[i].comments_count})
       </a>
     </li>`);
@@ -31,21 +34,28 @@ function newsFeed(){
 
 newsList.push('</ul>');
 
+newsList.push(`
+  <div>
+    <a href = "#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+    <a href = "#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}">다음 페이지</a>
+  </div>`
+  );
+
+
 container.innerHTML = newsList.join('') 
 //배열 요소 안에 들어있는 문자열을 하나로 합침 ,로 구분한다.
 // , 로 구분하지 않도록 빈 문자열로 join을 호출한다.  
 }
 
+// newsDetail
 function newsDetail(){
-  const id = location.hash.substr(1);
-
+  const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace('@id',id));
-  const title = document.createElement('h1');
 
   container.innerHTML = `
   <h1>${newsContent.title}</h1>
   <div>
-    <a href ="#">목록으로</a>
+    <a href ="#/page/${store.currentPage}">목록으로</a>
   </div>`;
   
 }
@@ -54,6 +64,9 @@ function router(){
   const routePath = location.hash;
 
   if(routePath === ''){
+    newsFeed();
+  } else if(routePath.indexOf('#/page/') >= 0){
+    store.currentPage = Number(routePath.substr(7));
     newsFeed();
   } else{
     newsDetail();
