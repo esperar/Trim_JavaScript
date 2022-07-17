3138c4c29cfe961dc780986614f60deb26aa9a32
KIMHUEMANG <piyrw9754@gmail.com>
2022-07-18 07:35:28 +0900

    swiper-practice

---
+++ swiper/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container',{
+  autoplay: true,
+  loop : true,
+  navigation : {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+  },
+  slidesPerView : 1,
+  slidesPerGroup: 1,
+});
+
